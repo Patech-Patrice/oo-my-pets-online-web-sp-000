e7503f65e4879e0ef26506ab33f0087108f5d74c
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-02 01:09:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,35 +1,72 @@
-class Owner
-   # code goes here	 
-    attr_accessor :name, :pets
+require 'pry'
+ class Owner	 class Owner
+   attr_accessor :pets, :count, :name
    attr_reader :species
  
     @@all = []
- 
-    def initialize(species)
-     @species = species
-     @@all << self
-     @pets = {
-       :fishes => [], 
-       :dogs => [], 
-       :cats => []
- 
-      }
-   end
  
     def self.all
      @@all
    end
  
+    def save
+     @@all << self
+   end
+ 
+  
+    def initialize(species= "human", name)
+     @species = species
+     @name = name
+     @pets = {fishes: [], cats: [], dogs: []}
+     @@all << self
+   end
+ 
+    def say_species
+     "I am a #{species}."
+   end
+ 
     def self.count
-     @@all.size
+     self.all.count
    end
  
     def self.reset_all
      @@all = []
    end
  
-    def say_species
-     "I am a #{species}."
+    def buy_fish(name)
+     @pets[:fishes] << Fish.new(name)
    end
-   
- end
+ 
+    def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+   end
+ 
+  
+    def buy_dog(name)
+     @pets[:dogs] << Dog.new(name)
+   end
+ 
+    def walk_dogs
+     @pets[:dogs].collect {|dog| dog.mood= "happy"}
+   end
+ 
+    def play_with_cats
+     @pets[:cats].collect {|cat| cat.mood= "happy"}
+   end
+ 
+    def feed_fish
+     @pets[:fishes].collect {|fish| fish.mood= "happy"}
+   end
+ 
+    def list_pets
+     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."    
+   end
+ 
+    def sell_pets
+     @pets.collect do |type, pets|
+       @pets[type].collect {|pet| pet.mood= "nervous"}
+     end
+     @pets.clear
+   end
+ 
+  end 	 
