007c8a81ada342b56522eb2dfc27ddff8f258bd6
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-02 00:51:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,14 +61,7 @@ class Owner
     end
   end
   
- def sell_pets
-     pets.each do |species, animals|
-       animals.each do |pet|
-         pet.mood = "nervous"
-       end
-       animals.clear
-     end
-   end
+
 
   def list_pets
      "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
